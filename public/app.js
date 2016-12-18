5b99827d56a31fcd97e934a0ac01787aa6b481d9
John Armbruster <jaboyak@gmail.com>
2016-12-18 16:17:00 -0700

    added comments and changed some html content

--- public/app.js
+++ public/app.js
@@ -1,4 +1,25 @@
+// After the HTML finishes loading, everything inside of this callback function is run.
 $(document).ready(function(){
-  // document.getElementById('the-foods-list').appendChild("<li>Pizza</li><li>Beer</li><li>Cake</li>")
+
+  /* 
+    For the element that has an id of 'the-foods-list', add this string of text after
+    the closing tag of the last element inside of said element. In our case, it would be added 
+    after the closing tag of the 'h3'
+
+    <ul id="the-foods-list">
+      <h3>Foods list: </h3>
+      * everything is added here... *
+    </ul>
+
+    TURNS INTO
+
+    <ul id="the-foods-list">
+      <h3>Foods list: </h3>
+      <li>Pizza</li>
+      <li>Beer</li>
+      <li>Cake</li>
+    </ul>
+
+  */
   $('#the-foods-list').append("<li>Pizza</li><li>Beer</li><li>Cake</li>");
 });
--- server.js
+++ server.js
@@ -1,24 +1,35 @@
+// Declare everything that you're going to use
 var express = require('express');
 var bodyParser =  require('body-parser');
 
+// Declare the instance of the server
 var app = express();
 
+// Turns the data that you send to the server into JSON
+// so you can see it on the 'request' object with 'request.body'
 app.use(bodyParser.json());
 
+// Serves all of the files in the '/public' directory
 app.use(express.static(__dirname + "/public"));
 
-// app.get('/', function(request, response) {
-//   response.send('this is my home page');
-// });
+// Creates an endpoint called '/test' that only accepts 'GET' requests, where if you 
+// went to 'localhost:3000/test', it would send you the string: 'this is my test page'
+app.get('/test', function(request, response) {
+  response.send('this is my test page');
+});
 
-// app.post('/', function(request, response) {
-//   var theInfoYouSentMe = request.body;
-//   theInfoYouSentMe.title = "changed!"
-//   console.log('request body', theInfoYouSentMe);
-//   response.send('you just send a post request');
-// });
+// Creates an endpoint called '/test' that only accepts 'POST' requests. Inside this endpoint, 
+// if you were to send the POST request with a body of { "title": "hello world" } and assign
+// it to the variable 'theInfoYouSentMe', line 25 would change the variable would change that
+// value to "changed!".
+app.post('/test', function(request, response) {
+  var theInfoYouSentMe = request.body;
+  theInfoYouSentMe.title = "changed!";
+  console.log('request body', theInfoYouSentMe);
+  response.send('you just send a post request');
+});
 
-
+// Starts the server on port 3001
 app.listen(3001, function() {
   console.log('server started');
 });
